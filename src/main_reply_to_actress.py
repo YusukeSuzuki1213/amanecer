8a5482b02a21fbc31a6f1b3e650ddf250f5e0887
YusukeSuzuki1213 <kikikaikai11110@gmail.com>
2022-03-10 13:33:39 +0900

    エラー処理

--- src/main_reply_to_actress.py
+++ src/main_reply_to_actress.py
@@ -1,17 +1,18 @@
-import atexit
 from di_module import DIModule
 from controller import Controller
 from injector import Injector
 from log import SlackClient
-
-
-def alert():
-    SlackClient().send_alert("main_actress_reply.pyのプログラムが停止しました")
+import traceback
 
 
 if __name__ == "__main__":
-    atexit.register(alert)
     injector = Injector([DIModule])
     controller = injector.get(Controller)
 
-    controller.reply_to_actress()
+    try:
+        controller.reply_to_actress()
+    except Exception as e:
+        print(traceback.format_exc())
+        print(e)
+        SlackClient().send_alert(traceback.format_exc())
+        SlackClient().send_alert(e)
